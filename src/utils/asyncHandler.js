57d910af7cb24801cc06e509e5d95d1da4ab0243
Shruti Dubey <shuruupmaneu@gmail.com>
2025-07-19 16:41:08 +0530

    Add user and video model

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -5,3 +5,20 @@ const asyncHandler = (requestHandler) => {
 }
 
 export {asyncHandler}
+
+
+
+
+
+
+
+// const asynHandler = (fn) = async (req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             message: err.message || 'Internal Server Error',
+//             success: false
+//         });
+//     }
+// }
